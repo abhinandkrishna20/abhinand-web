7cfd6fb2ddc2ef2876ca78c5bf9ba3df270689a9
Abhinand Krishna <am.abhinandkrishna@gmail.com>
2022-11-12 19:48:39 +0000

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,10 +45,6 @@ const Contact = () => {
               cols={40}
             />
           </div>
-
-          <div className="row">
-            <div className="col-md-4"></div>
-          </div>
         </div>
       </div>
     </div>
