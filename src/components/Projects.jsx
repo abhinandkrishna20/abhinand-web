9866d08f452013fbd34a325539fd27cd4c78dd75
Abhinand Krishna <am.abhinandkrishna@gmail.com>
2022-11-12 18:06:44 +0000

    Update 3 files

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,34 @@ const Projects = () => {
   return (
     <div>
       <h2>Projects</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="box0">
+              <div className="row">
+                <div className="col-md-6">date</div>
+                <div className="col-md-6">star</div>
+              </div>
+              <div className="h5">Project Name</div>
+
+              <div className="row">
+                <button
+                  className="col-md-4"
+                  style={{ "content-align": "left" }}
+                >
+                  Demo
+                </button>
+                <button
+                  className="col-md-4"
+                  style={{ "content-align": "left" }}
+                >
+                  Sourc
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
